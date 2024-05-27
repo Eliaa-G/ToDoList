f228e7953bf3ed70510156250bc9d4c59984ec67
Elia <elia28@live.com>
2024-05-27 18:06:16 +0200

    Initialized MVC

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -2,7 +2,14 @@ package todolist;
 
 public class Main {
     public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View(model);
+        Controller controller = new Controller(model, view);
         
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                view.setVisible(true);
+            }
+        });
     }
-    
 }
